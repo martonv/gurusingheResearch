249af8461f4f3abe9e2838b5ec1d02cdfcef5838
martonv <161093437+martonv@users.noreply.github.com>
2024-03-27 19:21:24 -0500

    3-27Final

--- calibration.py
+++ calibration.py
@@ -5,6 +5,13 @@ import numpy as np
 import pandas as pd
 import threading
 import inquirer
+#for valon:
+import serial
+from serial.tools import list_ports
+import time
+#for zaber:
+from zaber.serial import AsciiSerial, AsciiDevice, AsciiCommand, AsciiReply
+
 
 #notes:
 #ESR giving command error "5": Command Error. Shows that an error occurred while the
@@ -14,6 +21,9 @@ visa_address_Scope = 'TCPIP0::169.254.23.223::inst0::INSTR'
 visa_address_SRS = 'GPIB0::9::INSTR'
 
 channel = 'CH1'
+
+delay = 0.1
+port_name = "COM3"
 
 #initializing scope
 def initializeScope():
@@ -29,12 +39,100 @@ def initializeScope():
     r = oscilloScope.query('*opc?') # sync
     print(r)
     oscilloScope.write('*cls')
+    print("Scope opened successfully.")
 
 #initializing SRS triggering
 def initializeSRS():
     global SRS
     rm = visa.ResourceManager()
     SRS = rm.open_resource(visa_address_SRS)
+    print("SRS opened successfully.")
+
+def initializeValon(port):
+    global valonConnect
+
+    #creating class of valonConnection
+    class VSerialPort(serial.Serial):
+        def __init__( self, portParam = None):
+            serial.Serial.__init__(self)
+            # try:
+            self.baudrate = 9600
+            self.timeout = 3
+            self.port = port_name
+            self.open()
+            self.setDTR(False)
+            self.flushInput()
+            self.setDTR(True)
+            self.write(b'ID?\r')
+            response_bytes = self.read(1024) #total bytes expected back from return
+            print(response_bytes)
+
+            print(f"Serial port {port_name} opened successfully.")
+            time.sleep(1.0)
+            # except self.SerialException as e:
+            #     print(f"Error opening serial port: {e}")
+
+    valonConnect = VSerialPort(port)
+    
+    return valonConnect
+        
+def initializeZaber():
+    global zaberDevice
+
+    with AsciiSerial("COM4") as port:
+        zaberDevice = AsciiDevice(port, 1)
+        reply = zaberDevice.home()
+        if checkZaberCommand(reply):
+            print("Device homed.")
+        else:
+            print("Device home failed.")
+            exit(1)
+
+#ensures that zaber command success
+def checkZaberCommand(reply):
+    """
+    Return true if command succeeded, print reason and return false if command
+    rejected
+
+    param reply: AsciiReply
+
+    return: boolean
+    """
+    if reply.reply_flag != "OK": # If command not accepted (received "RJ")
+        print ("Danger! Command rejected because: {}".format(reply.data))
+        return False
+    else: # Command was accepted
+        return True
+        
+#write Valon cmds
+def writeValonCommand(cmd):
+    formatCmd = f"{cmd}"
+    valonConnect.reset_input_buffer()
+
+    #testing with query, splitting strings to do so
+    # splitCmd = formatCmd.replace('<', ' ')
+    # queryCmd = splitCmd.split(' ')[0] + '?\r'
+    # print(queryCmd)
+    # valonConnect.write(queryCmd.encode())
+    # time.sleep(delay)
+    # response_bytes = valonConnect.read(1024)
+    # response = response_bytes.decode().strip()
+    # print(response)
+
+    #then ready to send cmd
+    formatCmd += "\r" 
+    valonConnect.reset_input_buffer()
+    print(formatCmd)
+    valonConnect.write(formatCmd.encode())
+    time.sleep(delay)
+    response_bytes = valonConnect.read(1024)
+    response = response_bytes.decode().strip()
+    print(response)
+
+#valon settings init
+def valonSettings():
+    writeValonCommand("MODe CW ")
+
     
 #hardcoded freq, hardcoded delay, starts triggering
 def startTriggerSRS():
@@ -53,7 +151,7 @@ def queryOscCmd(command):
     print(command, ": ", output)
     return output
 
-#writes command to Oscilloscope
+#writes command to SRS
 def writeSRSCmd(command):
     output = SRS.write(command)
     return output
@@ -74,11 +172,12 @@ def estabOscSettings():
 
     #hardcoding settings [for now]
     channel = 'CH1'
-    horScale = 'horScale'
-    sampleRate = '10E-6'
+    horScale = '5E-6'
+    sampleRate = '100E+6'
     termination = '50E+0'
     vScale = '400E-6'
     triggerLvl = '60E-03'
+    triggerSource = 'CH2'
 
     writeOscCmd(f'HORizontal:MODE:SCAle {horScale}') #horizontal scale, each div (*10)
     #scope.write('HORizontal:MODE:RECOrdlength 20E+3')
@@ -89,6 +188,7 @@ def estabOscSettings():
     writeOscCmd(f'{channel}:BANdwidth TWOfifty')
     writeOscCmd(f'{channel}:COUPling DC') #
     writeOscCmd(f'TRIGger:A:LEVel {triggerLvl}') #fixes trigger level
+    writeOscCmd(f'TRIGGER:A:EDGE:SOURCE {triggerSource}') #sets trigger source
 
 #establishes Math1, Math2, and Math settings
 def estabOscFFTSettings():
@@ -107,8 +207,9 @@ def estabOscFFTSettings():
     gateWidth = '100E-6'
     gatePos = '0'
     channel = 'CH1'
-    horScale = 'horScale'
-    sampleRate = '10E-6'
+    #original settings
+    horScale = '10E-6'
+    sampleRate = '100E+6'
     termination = '50E+0'
     vScale = '400E-6'
     triggerLvl = '60E-03'
@@ -116,7 +217,6 @@ def estabOscFFTSettings():
     #establish math
     writeOscCmd(f'{mathChannel}:DEFINE "SpectralMag(AVG({channel}))"')
     writeOscCmd(f'SELECT:{mathChannel} 1')
-    print('testFF')
 
     writeOscCmd(f'HORizontal:MODE:SCAle {horScale}') #horizontal scale, each div (*10)
     #scope.write('HORizontal:MODE:RECOrdlength 20E+3')
@@ -127,6 +227,7 @@ def estabOscFFTSettings():
     writeOscCmd(f'{channel}:BANdwidth TWOfifty')
     writeOscCmd(f'{channel}:COUPling DC') #
     writeOscCmd(f'TRIGger:A:LEVel {triggerLvl}') #fixes trigger level
+    writeOscCmd('TRIGGER:A:EDGE:SOURCE CH2') #sets trigger source
 
     #hardcoding settings [for now]
     writeOscCmd(f'{mathChannel}:NUMAVg {mathAverages}') #sets math averages
@@ -137,7 +238,7 @@ def estabOscFFTSettings():
     writeOscCmd(f'{mathChannel}:VERTICAL:AUTOSCALE {autoScale}') #removes autoscaling so vert scale can be set
     writeOscCmd(f'{mathChannel}:VERTICAL:SCALE {vScale}') #sets math channel vertical scale
     #writeOscCmd('MATH2:VERTICAL:AUTOSCALE 0')
-    writeOscCmd(f'MATH2:SPECTral:CENTER {centerFreq}')
+    #writeOscCmd(f'MATH2:SPECTral:CENTER {centerFreq}')
     r = queryOscCmd('*opc?') 
     #writeOscCmd(f'MATH2:SPECTral:SPAN {vSpan}')
     r = queryOscCmd('*opc?') 
@@ -214,8 +315,58 @@ def fftWaveformAcq():
     return bin_wave
 
 #curvestream
+#encdg set to fastest
+#display OFF will make it faster
 def contAcqWF():
+
+    #turn on settings
+    estabOscSettings()
+
+    # io config
+    writeOscCmd('header 0')
+    writeOscCmd('data:encdg FAStest')
+    writeOscCmd('data:source CH1') # channel
+    # writeOscCmd('data:start 1') # first sample
+    # recordLength = int(queryOscCmd('horizontal:recordlength?'))
+    # writeOscCmd('data:stop {}'.format(recordLength)) # last sample
+    writeOscCmd('wfmoutpre:byt_n 1') # 1 byte per sample
+
+    # acq config
+    writeOscCmd('acquire:state 0') # stop
+    writeOscCmd('acquire:STOPAfter RUNSTop') # cont
+    writeOscCmd('DISplay:WAVEform OFF')
+    writeOscCmd('curvestream?')
+    writeOscCmd('acquire:state 1') # run
+    #writeOscCmd('curvestream?')
+    # t5 = time.perf_counter()
+    # r = queryOscCmd('*opc?') # sync
+    # t6 = time.perf_counter()
+    #print('acquire time: {} s'.format(t6 - t5))
+
+
+    while True:
+        tx = time.perf_counter()
+        bin_wave = oscilloScope.query_binary_values('curve?', datatype='b', container=np.array)
+        print(max(bin_wave))
+        ty = time.perf_counter()
+        diff = ty-tx
+        print(f'time: {diff}')
+    #temp = oscilloScope.query_binary_values('curve?', datatype='b', container=np.array)
+
+    # x = True
+    # while x == True:
+    #     bin_wave = oscilloScope.query_binary_values('curvestream?', datatype='b', container=np.array)
+    #     temp = oscilloScope.query_binary_values('curve?', datatype='b', container=np.array)
+
+    #     bin_wave = np.array(bin_wave, dtype='float')
+    #     temp = np.array(temp, dtype='float')
+
+    #     if bin_wave != temp:
+    #         print(bin_wave)
+    #     elif bin_wave == temp:
+    #         x = False
     return
+
 
 #get x and y vals from FFT waveform
 def scaleFFT(waveValues):
@@ -255,9 +406,7 @@ def generatePlot(xWave, yWave):
     plt.show()
     return
 
-def contAcq():
-    return
-
+#gets mode from user for acq type
 def mode():
     global answers
     questions = [
@@ -268,7 +417,6 @@ def mode():
 
     answers = inquirer.prompt(questions)
     return answers
-
 
 #if called, this function runs wf from scope and fft on PC
 def waveformPCFFT():
@@ -289,15 +437,67 @@ def waveformPCFFT():
     generatePlot(scaledTime, scaledWave)
     generatePlot(fftXValues, fftYValues)
 
+#gets fftWF from scope 
+def fftFromScope():
+    #estabOscSettings()
+    estabOscFFTSettings()
+    grabParam()
+
+    #acquire waveform
+    waveValues = fftWaveformAcq()
+
+    #acquire vals
+    xValues, yValues = scaleFFT(waveValues)
+
+    generatePlot(xValues, yValues)
+
+def zaberInput():
+    global zaberStep, zaberVel, zaberStart, zaberDist
+    zaberStep = input("Set Zaber step size (mm): ")
+    zaberVel = input("Set Zaber velocity (mm/s): ")
+    zaberStart = input("Set Zaber start pos (0 if 0mm) (mm): ")
+    zaberDist = input("Set Zaber end distance (mm): ")
+    zaberDevice.move_rel(zaberStart*20997)
+    zaberDevice.move_vel(zaberVel*20997)   
+
+def valonInput():
+    global valonFreq, valonFreqStep, valonTotalSteps
+    valonFreq = input("Starting frequency? (MHz): ")
+    writeValonCommand(f"Frequency {valonFreq}M")
+    valonFreqStep = input("Freq step size? (MHz): ")
+    writeValonCommand(f"STEP {valonFreqStep}M")
+    valonTotalSteps = input("How many steps? ")
+    writeValonCommand(f"STEP {valonTotalSteps}M")
 
 def main():
+    global modeSetting, valonConnect
+
     initializeScope()
-    initializeSRS()
+    #initializeSRS()
+    #valonConnect = initializeValon('COM3')
+    #valonSettings()
+    #initializeZaber()
+
+    #getting current mode: 'Waveform/PCFFT', 'FFTWF', 'ContWF', 'ContFFT'
     modeSetting = mode()
+    print(modeSetting)
     
-    match modeSetting:
+    match modeSetting['mode']:
         case 'Waveform/PCFFT':
             waveformPCFFT()
+        case 'FFTWF':
+            fftFromScope()
+        case 'ContWF':
+            #contAcqWF()
+            nothing = 1
+        case 'ContFFT':
+            nothing = 1
+
+    #user inputs        
+    # valonInput()
+    # zaberInput() 
+    
+      
 
     # modeSRS = input("Would you like to trigger via SRS? Y/N: ")
     # if modeSRS == 'Y':
@@ -320,5 +520,8 @@ def main():
 
 if __name__ == "__main__":
     main()
-    t1 = threading.Thread(target=contAcq)
-    t1.da
+    if modeSetting['mode'] == 'contWF' or modeSetting == 'contFFT':
+        t1 = threading.Thread(target=contAcqWF)
+        t1.start()
+
+
